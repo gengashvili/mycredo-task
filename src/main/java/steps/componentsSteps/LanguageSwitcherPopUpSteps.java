7df8372fc4acae516e24def0ff202016f99d25bc
giorgi gengashvili <giorgi.gengashvili682@eab.tsu.ge>
2025-06-12 21:12:04 +0400

    feat(page-object-model): add steps for auth page and language switcher pop up

--- src/main/java/steps/componentsSteps/LanguageSwitcherPopUpSteps.java
+++ src/main/java/steps/componentsSteps/LanguageSwitcherPopUpSteps.java
@@ -1,7 +1,24 @@
 package steps.componentsSteps;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
 import pages.components.LanguageSwitcherPopUp;
 
-public class LanguageSwitcherPopUpSteps {
+public class LanguageSwitcherPopUpSteps extends LanguageSwitcherPopUp {
+    private SoftAssert softAssert;
+
+    public LanguageSwitcherPopUpSteps(WebDriver driver, WebDriverWait wait, SoftAssert softAssert) {
+        super(wait);
+        this.softAssert = softAssert;
+    }
+
+    @Step("Switch language to: {language}")
+    public LanguageSwitcherPopUpSteps switchLanguage(String language) {
+        getLanguageButton(language).click();
+
+        return this;
+    }
 
 }
